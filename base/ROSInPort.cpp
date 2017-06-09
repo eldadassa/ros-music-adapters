ac553d11d074dfa9ce8acc2725faaba17cbd9424
Philipp Weidel <p.weidel@fz-juelich.de>
2017-06-09 21:17:08 +0200

    compilable with catkin

--- base/ROSInPort.cpp
+++ base/ROSInPort.cpp
@@ -4,7 +4,10 @@
 void
 ROSInPort::init(MUSIC::Setup* setup, char* port_name)
 {
-    ros::init(setup->argc, setup->argv, port_name);
+    //TODO pass real argv, argc??
+    int argc = 1;
+    char *argv = new char[0];
+    ros::init(argc, &argv, port_name);
     ros::start();
 
     ros::NodeHandle n;
